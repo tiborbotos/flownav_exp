99a8276039076085a845b99892db865be5a38d39
Tibor Botos <tibor.botos@gawker.com>
2015-01-28 17:29:46 +0100

    Get to the bottom

--- js/main.js
+++ js/main.js
@@ -12,9 +12,9 @@
 	'use strict';
 
 	var topNavSelector	= '.flownav-top',
-		bottomNavSelector = '',
+		bottomNavSelector = '.flownav-bottom',
 		$topNav		= $( topNavSelector),
-		$bottomNav  = $( topNavSelector);
+		$bottomNav  = $( bottomNavSelector);
 
 	if( !$topNav.length ) return true;
 
@@ -26,19 +26,24 @@
 		wHeight			= 0,
 		wScrollCurrent	= 0,
 		wScrollBefore	= 0,
-		wScrollDiff		= 0;
+		wScrollDiff		= 0,
+		bottomNavHeight = 0,
+		bottomNavBottom = -100;
 
 	$window.on( 'scroll', function()
 	{
-		elHeight		= $topNav.outerHeight();
+		elHeight		= $topNav.outerHeight() + 10;
 		dHeight			= $document.height();
 		wHeight			= $window.height();
 		wScrollCurrent	= $window.scrollTop();
 		wScrollDiff		= wScrollBefore - wScrollCurrent;
 		elTop			= parseInt( $topNav.css( 'top' ) ) + wScrollDiff;
+		bottomNavHeight = $bottomNav.outerHeight(),
+		bottomNavBottom = parseInt($bottomNav.css('bottom'));
 
 		if( wScrollCurrent <= 0 ) {// scrolled to the very top; element sticks to the top
 			$topNav.css( 'top', 0 );
+			$bottomNav.css('bottom', -100);
 		}
 		else if( wScrollDiff > 0 ) { // scrolled up; element slides in
 			$topNav.css( 'top', elTop > 0 ? 0 : elTop );
@@ -51,6 +56,11 @@
 			else // scrolled down; element slides out
 				$topNav.css( 'top', Math.abs( elTop ) > elHeight ? -elHeight : elTop );
 		}
+
+		if (wScrollCurrent > elHeight && wScrollDiff < 0 && bottomNavBottom < 0) {
+			$bottomNav.css('bottom', 0);
+		}
+
 
 		wScrollBefore = wScrollCurrent;
 	});
