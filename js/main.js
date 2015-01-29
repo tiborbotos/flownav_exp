c52dbb47bae5f75a47d78fd7252fd95d1659554b
Tibor Botos <tibor.botos@gawker.com>
2015-01-29 12:22:33 +0100

    More animation

--- js/main.js
+++ js/main.js
@@ -11,15 +11,18 @@
 {
 	'use strict';
 
-	var topNavSelector	= '.flownav-top',
-		bottomNavSelector = '.flownav-bottom',
-		$topNav		= $( topNavSelector),
-		$bottomNav  = $( bottomNavSelector);
+	var topNavSelector		= '.flownav-top',
+		bottomNavSelector 	= '.flownav-bottom',
+		$topNav				= $( topNavSelector),
+		$topNavMenu 		= $( topNavSelector + ' .menu'),
+		$bottomNav  		= $( bottomNavSelector);
 
 	if( !$topNav.length ) return true;
 
-	var elHeight		= 0,
-		elTop			= 0,
+	var bottomPosInvisible = -100;
+
+	var topNavHeight	= 0,
+		topNavTop		= 0,
 		$document		= $( document ),
 		dHeight			= 0,
 		$window			= $( window ),
@@ -28,37 +31,70 @@
 		wScrollBefore	= 0,
 		wScrollDiff		= 0,
 		bottomNavHeight = 0,
-		bottomNavBottom = -100;
+		bottomNavBottom = -100,
+		articleTop 		= $('.content').offset().top,
+		articleBottom	= $('.comments').offset().top,
+		topNavScrolledAwayOnce = false;
 
 	$window.on( 'scroll', function()
 	{
-		elHeight		= $topNav.outerHeight() + 10;
-		dHeight			= $document.height();
-		wHeight			= $window.height();
-		wScrollCurrent	= $window.scrollTop();
-		wScrollDiff		= wScrollBefore - wScrollCurrent;
-		elTop			= parseInt( $topNav.css( 'top' ) ) + wScrollDiff;
-		bottomNavHeight = $bottomNav.outerHeight(),
-		bottomNavBottom = parseInt($bottomNav.css('bottom'));
+		topNavHeight		= $topNav.outerHeight() + 10;
+		dHeight				= $document.height();
+		wHeight				= $window.height();
+		wScrollCurrent		= $window.scrollTop();
+		wScrollDiff			= wScrollBefore - wScrollCurrent;
+		topNavTop			= parseInt( $topNav.css( 'top' ) ) + wScrollDiff;
+		bottomNavHeight		= $bottomNav.outerHeight(),
+		bottomNavBottom		= parseInt($bottomNav.css('bottom'));
 
-		if( wScrollCurrent <= 0 ) {// scrolled to the very top; element sticks to the top
+		if( wScrollCurrent <= 0 ) {// scrolled to the very top; top nav sticks to the top
 			$topNav.css( 'top', 0 );
-			$bottomNav.css('bottom', -100);
-		}
-		else if( wScrollDiff > 0 ) { // scrolled up; element slides in
-			$topNav.css( 'top', elTop > 0 ? 0 : elTop );
-		}
-		else if( wScrollDiff < 0 ) {// scrolled down
+			$bottomNav.css('bottom', bottomPosInvisible);
 
-			if( wScrollCurrent + wHeight >= dHeight - elHeight )  // scrolled to the very bottom; element slides in
-				$topNav.css( 'top', ( elTop = wScrollCurrent + wHeight - dHeight ) < 0 ? elTop : 0 );
+			if (topNavScrolledAwayOnce) {
+				$topNav.animate({ height : 110 }, 200);
+				$topNavMenu.slideDown(200);
+				topNavScrolledAwayOnce = false;
+			}
+		}
+		else if( wScrollDiff > 0 ) { // scrolled up; top nav slides in
 
-			else // scrolled down; element slides out
-				$topNav.css( 'top', Math.abs( elTop ) > elHeight ? -elHeight : elTop );
+			if (wScrollDiff < 3) { // slow scroll
+
+				if (wScrollCurrent < articleTop || wScrollCurrent > articleBottom) { // we don't want to interrupt the user with the top nav (he's reading)
+					$topNav.css( 'top', topNavTop > 0 ? 0 : topNavTop );
+				}
+
+			} else if (wScrollDiff < 7) { // medium scroll, top nav shows up by scrolling, bottom nav stays visible
+
+				$topNav.css( 'top', topNavTop > 0 ? 0 : topNavTop );
+
+			} else { // fast scroll, top nav shows up fast, bottom nav disappers
+
+				$topNav.css( 'top', topNavTop > 0 ? 0 : topNavTop );
+				$bottomNav.css('bottom', bottomPosInvisible);
+			}
+
+		}
+		else if( wScrollDiff < 0 ) { // scrolled down
+
+			if( wScrollCurrent + wHeight >= dHeight - topNavHeight ) { // scrolled to the very bottom; top nav slides in
+				$topNav.css('top', ( topNavTop = wScrollCurrent + wHeight - dHeight ) < 0 ? topNavTop : 0);
+			}
+			else { // scrolled down; element slides out
+				$topNav.css( 'top', Math.abs( topNavTop ) > topNavHeight ? -topNavHeight : topNavTop );
+
+				if (parseInt($topNav.css('top')) <= -topNavHeight && !topNavScrolledAwayOnce) { // set the flag and hide the top nav menu
+					topNavScrolledAwayOnce = true;
+					$topNavMenu.hide();
+					$topNav.height(60);
+				}
+			}
 		}
 
-		if (wScrollCurrent > elHeight && wScrollDiff < 0 && bottomNavBottom < 0) {
+		if (wScrollCurrent > topNavHeight && wScrollDiff < 0 && bottomNavBottom < 0) { // bottom nav appears if top nav is not visible
 			$bottomNav.css('bottom', 0);
+			scrolledToTop();
 		}
 
 		wScrollBefore = wScrollCurrent;
@@ -67,18 +103,20 @@
 })( jQuery, window, document );
 
 
+function scrolledToTop() {
+	$('.js_trending span').addClass('active');
+}
 
+
+// ACTIONS
 ( function ($) {
 
 	$('.js_move-top').click(function () {
-		$('html, body').animate(
-			{
-				scrollTop : 0
-			},
-			{
-				duration: 600,
-				easing: 'easeOutQuart'
-			});
+		$('html, body').animate({ scrollTop : 0 }, { duration: 600, easing: 'easeOutQuart' });
+	});
+
+	$('.js_recommend').click(function () {
+		$('.js_recommend span').toggleClass('active');
 	});
 
 })(jQuery);
